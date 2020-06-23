a0b722d3818c0d9324cc70f91a6dc0e1d433c048
Alex Nagy <therebeloper@gmail.com>
2020-06-23 06:53:54 +0300

    Initial Commit

---
+++ BetterCodableDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BetterCodableDemo
+//
+//  Created by Alex Nagy on 23/06/2020.
+//  Copyright © 2020 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
